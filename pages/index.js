f3fcbe336df0d4269e87f753f0769e8113481ae9
KomilovQobiljon <komilov19inha@gmail.com>
2022-12-10 02:58:34 +0900

    created search bar in widgets

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
+import Widgets from '../components/Widgets';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Feed />
 
       {/* Widgets */}
+      <Widgets />
 
       {/* Modal */}
     </div>
